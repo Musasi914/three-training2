d81a2bbd4b7478e12a55b5a25352b29fb6ad6824
Hidetaka Matsuda <sopmod120@gmail.com>
2025-12-06 14:13:51 +0900

    feat: glob依存関係を追加し、動的HTML入力処理のためのVite設定を更新する

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,36 @@
 import { defineConfig } from "vite";
-import { resolve } from "path";
+import path, { resolve } from "path";
 import glsl from "vite-plugin-glsl";
+import { fileURLToPath } from "url";
+import { glob } from "glob";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export default defineConfig({
+  root: resolve(__dirname, "./"),
   resolve: {
     alias: {
       "@shared": resolve(__dirname, "./src/shared"),
     },
   },
   build: {
+    outDir: resolve(__dirname, "./dist"),
     commonjsOptions: {
       include: [/node_modules/],
+    },
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        ...Object.fromEntries(
+          glob
+            .sync("src/examples/**/index.html")
+            .map((file) => [
+              file.replace("src/", "").replace("/index.html", ""),
+              resolve(__dirname, file),
+            ])
+        ),
+      },
     },
   },
   plugins: [glsl()],
